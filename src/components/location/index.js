808a2d0805e6f7d1243f7ab05c13f180ee4c214d
David Ewan Campbell <ewan.david@gmail.com>
2022-06-01 23:30:14 +0100

    location accept and display user input

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -1,42 +1,47 @@
 import React from 'react';
 // import './index.css'
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import GeolocationAPI from '../../hooks/geocodingAPI';
 import BirdAPI from '../../hooks/birdAPI';
 import PlantAPI from '../../hooks/plantAPI';
 import WikiDescriptionAPI from '../../hooks/wikipediaDescriptionAPI';
 import WikiImageAPI from '../../hooks/wikipediaImageAPI'
 
-
-
-const Location = ({postcode}) => {
+const Location = () => {
+  const [ postcode, setPostcode ] = useState('');
   // const [geolocation] = GeolocationAPI(postcode);
   // const [birds] = BirdAPI([geolocation])
   // const [plants] = PlantAPI([geolocation])
   // console.log('1')
   // const [description] = WikiDescriptionAPI()
   
-const handleSubmit = () => {
+const handleSubmit = (event) => { 
+  event.preventDefault();
+  setPostcode(event.target.value);
   console.log('2');
   console.log(postcode);
   const [geolocation] = GeolocationAPI(postcode);
-  console.log('3');
-  console.log(geolocation);
-  const [birds] = BirdAPI([geolocation])
-  console.log(birds);
-  const [plants] = PlantAPI([geolocation])
-  console.log(plants);
+  // console.log('3');
+  // console.log(geolocation);
+  // const [birds] = BirdAPI([geolocation])
+  // console.log(birds);
+  // const [plants] = PlantAPI([geolocation])
+  // console.log(plants);
 }
+
+const handleChange = (event) => {setPostcode(event.target.value)};
 
   return (
     <div className="location">
-      <form onSubmit={handleSubmit()} className="location-form">
+      <form onSubmit={handleSubmit} className="location-form">
         <input
           id="postcode"
           className="form-field"
           type="text"
+          value={postcode}
           placeholder="Enter postcode here..."
           postcode="postcode"
+          onChange={handleChange} 
         />
         <span id="postcode-error">Please enter a valid postcode</span>
         <button className="form-field" type="submit">

319c1124b5c0a016155e72d79eae0a337cef655a
Rupert Pople <rupertpople@gmail.com>
2022-06-01 17:18:50 +0100

    attmepting to figure out handle submit

--- src/components/location/index.js
+++ src/components/location/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import './index.css'
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import GeolocationAPI from '../../hooks/geocodingAPI';
 import BirdAPI from '../../hooks/birdAPI';
 import PlantAPI from '../../hooks/plantAPI';
@@ -10,33 +10,31 @@ import WikiImageAPI from '../../hooks/wikipediaImageAPI'
 
 
 const Location = ({postcode}) => {
-  // const [geolocation] = GeolocationAPI(postcode);
-  // const [birds] = BirdAPI([geolocation])
-  // const [plants] = PlantAPI([geolocation])
-  // console.log('1')
-  // const [description] = WikiDescriptionAPI()
-  
+
+  const [location, setLocation] = useState("")
+
 const handleSubmit = () => {
-  console.log('2');
-  console.log(postcode);
-  const [geolocation] = GeolocationAPI(postcode);
-  console.log('3');
-  console.log(geolocation);
-  const [birds] = BirdAPI([geolocation])
-  console.log(birds);
-  const [plants] = PlantAPI([geolocation])
-  console.log(plants);
+  console.log('1')
+  console.log(location)
+  const [geocoding] = GeolocationAPI('w149ef')
+  console.log('2')
+  console.log(geocoding)
+  setLocation(geocoding)
+  console.log('3')
+  console.log(location)
 }
 
   return (
     <div className="location">
-      <form onSubmit={handleSubmit()} className="location-form">
+      <form className="location-form" onSubmit={handleSubmit()}>
         <input
           id="postcode"
           className="form-field"
           type="text"
           placeholder="Enter postcode here..."
           postcode="postcode"
+          value={location}
+          onChange={(e) => setLocation(e.target.value) }
         />
         <span id="postcode-error">Please enter a valid postcode</span>
         <button className="form-field" type="submit">

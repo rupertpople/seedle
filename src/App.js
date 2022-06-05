19b819c998af41404b19f1c1737b85ee4eef4389
Coral-Ann <coral.baker@outlook.com>
2022-06-05 14:34:05 +0100

    First css edit.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
 import React from 'react'
 import Location from './components/location';
 import Post from './components/post';
-//import PlantModel from './model/plantModel';
-//import PlantAPI from './model/plantAPI';
 
-//const plants = new PlantModel(new PlantAPI('lat=51.4890&lon=-0.2122'));
 
 const App = () => {
   return (
     <div className="App">
-      <h2>Welcome to Seedle!</h2>
+      <div className="heading"><img src="https://i.imgur.com/pbbU5dm.png" width="400px" alt="seedlelogo"></img>
       <h3>Your Online Local Species Explorer </h3>
+      </div>
       <Post />
       <Location index={1}/> 
     </div>

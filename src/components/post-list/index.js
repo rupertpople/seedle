430548019bc377cfd659740c4808378a47bf45c6
Rupert Pople <rupertpople@gmail.com>
2022-06-06 15:58:55 +0100

    display

--- src/components/post-list/index.js
+++ src/components/post-list/index.js
@@ -3,17 +3,11 @@ import Post from '../post';
 import Location from '../location';
 
 const PostList = (speciesInfo) => {
-  const [array, setArray] = useState(speciesInfo);
-
-  // const speciesPromise = Promise.all(speciesInfo);
-  // console.log(speciesPromise);
-
-  console.log(speciesInfo.speciesInfo[0])
+  const [array, setArray] = useState(speciesInfo.speciesInfo[0]);
   
   return (
     <div className="post-list">
-      <p> Hi hello howdy howdo hoi</p>
-      {speciesInfo.speciesInfo[0].map((species, index)=>{
+      {array.map((species, index)=>{
         return <div key={index}>{species.commonName}</div>;
       })}
     </div>
